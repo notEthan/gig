3288f310cf5a5381ea9069c0b5039e662f5f3db6
Ethan <ethan@unth.net>
2023-01-10 12:39:25 -0800

    improve and regroup gig task
    check spec+ignore regardless of git/fs
    don't check whether files that are ignored are in git
    checking gemspec and ignore is outermost, that being most important distinction; checking git and filesystem below those.

--- lib/gig.rb
+++ lib/gig.rb
@@ -33,33 +33,36 @@ module Gig
           in_spec = spec.files.include?(file) || spec.test_files.include?(file)
           in_ignore = ignore_files.include?(file)
 
-          if in_git
-            if in_fs
-              if in_spec
-                if in_ignore
-                  file_error.("file is ignored, but present in gemspec: #{file}")
-                end
+          if in_spec
+            if in_ignore
+              file_error.("file in gig ignore_files, but present in gemspec: #{file}")
+            end
+
+            if in_git
+              if in_fs
                 git_status = `git status --porcelain #{Shellwords.escape(file)}`
                 if git_status.empty?
                   # pass
                 else
-                  file_error.("file modified from git: #{file}")
+                  file_error.("file in gemspec, but modified from git: #{file}")
                 end
               else
-                if in_ignore
-                  # pass
-                else
-                  file_error.("git file not in gemspec: #{file}")
-                end
+                file_error.("file in gemspec, but not in filesystem: #{file}")
               end
             else
-              file_error.("git file not in fs: #{file}")
+              if in_fs
+                file_error.("file in gemspec and filesystem, but not in git: #{file}")
+              else
+                file_error.("file in gemspec, but not in git and not in filesystem: #{file}")
+              end
             end
+          elsif in_ignore
+            # in ignore_files, and not in gemspec: pass (regardless of git or fs)
           else
-            if in_spec
-              file_error.("file in gemspec but not in git: #{file}")
+            if in_git
+              file_error.("file in git, but in neither gemspec nor gig ignore_files: #{file}")
             else
-              # in fs but ignored by git and spec: pass
+              # not in gemspec, not in ignore_files, not in git: pass - it's in the filesystem but can be ignored
             end
           end
         end
